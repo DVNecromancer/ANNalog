d75a4687286c8b88cd3f7abb330c8058adf5cbdf
DVNecromancer <97798602+DVNecromancer@users.noreply.github.com>
2026-03-14 12:24:03 +0000

    Update chembl-gen-check version to 0.1.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     },
     install_requires=[
         "partialsmiles>=2.0",
-        "chembl-gen-check==0.1.2",
+        "chembl-gen-check==0.1.3",
     ],
     python_requires=">=3.10",
     entry_points={
